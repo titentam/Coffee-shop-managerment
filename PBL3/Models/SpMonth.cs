670c5c83d7654fa641d9aa1d80d787f70067abdd
Tâm Phan <phantam04012003@gmail.com>
2023-04-15 18:36:41 +0700

    Chỉnh home

---
+++ PBL3/Models/SpMonth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PBL3.Models
+{
+    [Keyless]
+    public class SpMonth
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public decimal Total { get; set; }
+    }
+}
