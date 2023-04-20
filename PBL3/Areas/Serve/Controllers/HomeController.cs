f76060492a838fedda9f7564a26d69081f0f08f0
duypro432003 <vanduyluu432003@gmail.com>
2023-04-20 15:29:24 +0700

    update phuc vu

--- PBL3/Areas/Serve/Controllers/HomeController.cs
+++ PBL3/Areas/Serve/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using PBL3.Model;
+using PBL3.Models;
 
 namespace PBL3.Areas.Serve.Controllers
 {
     [Area("Serve")]
     public class HomeController : Controller
     {
+        private readonly TamtentoiContext _context = new TamtentoiContext();
+        
         public IActionResult Index()
         {
-            return View();
+            var list = _context.Mons.OrderBy(n => n.MonId).ToList();
+            return View(list);
         }
     }
 }
--- PBL3/Models/TamtentoiContext.cs
+++ PBL3/Models/TamtentoiContext.cs
@@ -27,7 +27,7 @@ namespace PBL3.Models
 
         public virtual DbSet<SpMonth> SpMonths { get; set; } = null!;
         public virtual DbSet<SpYear> SpYears { get; set; } = null!;
-		public virtual DbSet<Ban> Bans { get; set; } = null!;
+        public virtual DbSet<Ban> Bans { get; set; } = null!;
         public virtual DbSet<CaLam> CaLams { get; set; } = null!;
         public virtual DbSet<CongThuc> CongThucs { get; set; } = null!;
         public virtual DbSet<CongThucNguyenLieu> CongThucNguyenLieus { get; set; } = null!;
@@ -49,7 +49,7 @@ namespace PBL3.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=msi\\sqlexpress01;Database=Tamtentoi;Trusted_Connection=True;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-CUA-DUY\\SQLEXPRESS02;Database=Tamtentoi;Trusted_Connection=True;Integrated Security=True;");
             }
         }
 
@@ -397,6 +397,6 @@ namespace PBL3.Models
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 
-        
+
     }
 }
