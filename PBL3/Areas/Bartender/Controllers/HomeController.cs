e8ff045f9416d4741cda08d0b315ab4a6402530b
duypro432003 <vanduyluu432003@gmail.com>
2023-04-29 21:01:20 +0700

    commit

--- PBL3/Areas/Bartender/Controllers/HomeController.cs
+++ PBL3/Areas/Bartender/Controllers/HomeController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PBL3.Models;
+using System.Web;
 
 namespace PBL3.Areas.Bartender.Controllers
 {
     [Area("Bartender")]
     public class HomeController : Controller
     {
+        private readonly TamtentoiContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public INotyfService _notifyService { get; }
+        public HomeController(TamtentoiContext context, INotyfService notifyService)
+        {
+            _context = context;
+            _notifyService = notifyService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+           var list= _context.DonDatMons.ToList();
+            
+            return View( list);
         }
+
     }
 }
