b9320b10f1755dd6d42c4e1194c83222cf2f8767
duypro432003 <vanduyluu432003@gmail.com>
2023-04-30 21:27:11 +0700

    thêm danh sách nguyên liệu,xử lí sự kiện hoàn thành, xem công thức, hủy đơn

--- PBL3/Areas/Bartender/Controllers/HomeController.cs
+++ PBL3/Areas/Bartender/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace PBL3.Areas.Bartender.Controllers
 
         public IActionResult Index()
         {
-            var list = _context.DonDatMons.Where(x=>x.TinhTrang==false).ToList();
+            var list = _context.DonDatMons.Where(x=>x.TinhTrang==false ).ToList();
 
             return View(list);
         }
@@ -27,7 +27,7 @@ namespace PBL3.Areas.Bartender.Controllers
         public IActionResult ShowDanhSach(int id)
         {
             
-            var listOrderDetail = _context.MonDonDatMons.Where(x=>x.DonDatMonId==id).ToList();
+            var listOrderDetail = _context.MonDonDatMons.Where(x=>x.DonDatMonId==id ).ToList();
             List<Tuple<Mon, int>> listItem = new List<Tuple<Mon, int>>();
             foreach(var item in listOrderDetail)
             {
@@ -55,6 +55,61 @@ namespace PBL3.Areas.Bartender.Controllers
                 }
             }    
             return PartialView("_PartialViewShowCongThuc", listCTDetails);
+        }
+        public IActionResult ShowNguyenLieu()
+        {
+
+            var listNguyenLieu= _context.NguyenLieus.ToList();
+            return PartialView("_PartialViewShowNguyenLieu", listNguyenLieu);
+        }
+        [HttpPost]
+        public IActionResult HoanThanhDonHang(int monId)
+        {
+            
+            var mon = _context.Mons.Find(monId);
+            if (mon!=null)
+            {
+                var ListCT = _context.CongThucNguyenLieus.Where(x => x.CongThucId==mon.CongThucId).ToList();
+
+                foreach (var nguyenLieu in ListCT)
+                {
+                    var nguyenLieuTrongKho = _context.NguyenLieus
+                        .FirstOrDefault(x => x.NguyenLieuId == nguyenLieu.NguyenLieuId);
+                    if (nguyenLieuTrongKho != null)
+                    {
+                        nguyenLieuTrongKho.SoLuong -= nguyenLieu.SoLuong;
+                    }
+                }
+                _context.SaveChanges();
+
+            }
+            // Trả về dữ liệu số lượng nguyên liệu còn lại trong kho
+            var soLuongNguyenLieuConLai = _context.NguyenLieus.Select(x => x.SoLuong);
+            return Json(new { soLuongNguyenLieuConLai });
+        }
+        public IActionResult HuyDonHang(int monId)
+        {
+            try
+            {
+                // Tìm kiếm đơn hàng tương ứng từ cơ sở dữ liệu
+                //var donhang = _context.DonHangs.FirstOrDefault(x => x.Id == id);
+
+                //// Nếu không tìm thấy đơn hàng, trả về kết quả lỗi
+                //if (donhang == null)
+                //    return Json(new { success = false, message = "Không tìm thấy đơn hàng." });
+
+                //// Cập nhật trạng thái của đơn hàng thành "Đã hủy"
+                //donhang.TrangThai = "DaHuy";
+                //_context.SaveChanges();
+
+                // Trả về kết quả thành công
+                return Json(new { success = true, message = "Hủy đơn hàng thành công!" });
+            }
+            catch (Exception ex)
+            {
+                // Nếu xảy ra lỗi, trả về kết quả lỗi
+                return Json(new { success = false, message = "Lỗi khi hủy đơn hàng: " + ex.Message });
+            }
         }
 
     }
