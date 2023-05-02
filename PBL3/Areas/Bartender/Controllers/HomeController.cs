2210d1290a440d3b9b86c4efbd58ba8a44e01eaf
Tâm Phan <phantam04012003@gmail.com>
2023-05-02 11:19:02 +0700

    fix pha chế

--- PBL3/Areas/Bartender/Controllers/HomeController.cs
+++ PBL3/Areas/Bartender/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace PBL3.Areas.Bartender.Controllers
 
         public IActionResult Index()
         {
-            var list = _context.DonDatMons.Where(x=>x.TinhTrang==false ).ToList();
+            var list = _context.DonDatMons.Where(x=>x.TinhTrang==false ).ToList(); // false: chưa làm, null: hủy
 
             return View(list);
         }
@@ -37,6 +37,8 @@ namespace PBL3.Areas.Bartender.Controllers
                     listItem.Add(new Tuple<Mon,int>(mon,(int)item.SoLuong));
                 }
             }
+            ViewBag.OrderId = id;
+            ViewBag.ListOrderDetail = listOrderDetail;
             return PartialView("_PartialViewDanhSachMon", listItem);
         }
         public IActionResult ShowCongThuc(int id)
@@ -63,53 +65,46 @@ namespace PBL3.Areas.Bartender.Controllers
             return PartialView("_PartialViewShowNguyenLieu", listNguyenLieu);
         }
         [HttpPost]
-        public IActionResult HoanThanhDonHang(int monId)
+        public IActionResult HoanThanhDonHang(int monId, int orderId)
         {
             
             var mon = _context.Mons.Find(monId);
+            string thongBao = "Thành công!";
+            bool check = true;
             if (mon!=null)
             {
                 var ListCT = _context.CongThucNguyenLieus.Where(x => x.CongThucId==mon.CongThucId).ToList();
-
-                foreach (var nguyenLieu in ListCT)
+                var orderDetail = _context.MonDonDatMons.Where(x => x.DonDatMonId == orderId && x.MonId == monId).SingleOrDefault();
+                
+                foreach (var ctNl in ListCT)
                 {
                     var nguyenLieuTrongKho = _context.NguyenLieus
-                        .FirstOrDefault(x => x.NguyenLieuId == nguyenLieu.NguyenLieuId);
-                    if (nguyenLieuTrongKho != null)
+                        .FirstOrDefault(x => x.NguyenLieuId == ctNl.NguyenLieuId);
+                    if (nguyenLieuTrongKho != null&& nguyenLieuTrongKho.SoLuong>=orderDetail.SoLuong* ctNl.SoLuong)
                     {
-                        nguyenLieuTrongKho.SoLuong -= nguyenLieu.SoLuong;
+                        nguyenLieuTrongKho.SoLuong -= orderDetail.SoLuong * ctNl.SoLuong;
+                        orderDetail.TinhTrang = true;
+                    }
+                    else
+                    {
+                        thongBao = "Nguyên liệu không đủ!";
+                        check = false;
+                        break;
                     }
                 }
                 _context.SaveChanges();
-
             }
-            // Trả về dữ liệu số lượng nguyên liệu còn lại trong kho
-            var soLuongNguyenLieuConLai = _context.NguyenLieus.Select(x => x.SoLuong);
-            return Json(new { soLuongNguyenLieuConLai });
+            return Json(new { thongBao, check });
         }
-        public IActionResult HuyDonHang(int monId)
+        public IActionResult HuyDonHang(int monId, int orderId)
         {
-            try
+            var orderDetail = _context.MonDonDatMons.Where(x=>x.MonId==monId&& x.DonDatMonId==orderId).FirstOrDefault();
+            if(orderDetail != null)
             {
-                // Tìm kiếm đơn hàng tương ứng từ cơ sở dữ liệu
-                //var donhang = _context.DonHangs.FirstOrDefault(x => x.Id == id);
-
-                //// Nếu không tìm thấy đơn hàng, trả về kết quả lỗi
-                //if (donhang == null)
-                //    return Json(new { success = false, message = "Không tìm thấy đơn hàng." });
-
-                //// Cập nhật trạng thái của đơn hàng thành "Đã hủy"
-                //donhang.TrangThai = "DaHuy";
-                //_context.SaveChanges();
-
-                // Trả về kết quả thành công
-                return Json(new { success = true, message = "Hủy đơn hàng thành công!" });
+                orderDetail.TinhTrang = false;
+                _context.SaveChanges();
             }
-            catch (Exception ex)
-            {
-                // Nếu xảy ra lỗi, trả về kết quả lỗi
-                return Json(new { success = false, message = "Lỗi khi hủy đơn hàng: " + ex.Message });
-            }
+            return Json(new {});
         }
 
     }
