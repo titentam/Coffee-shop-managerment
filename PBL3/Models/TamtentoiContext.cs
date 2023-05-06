71a9b0350bfbf14d87594b7abf205628f652ebab
duypro432003 <vanduyluu432003@gmail.com>
2023-05-06 15:31:07 +0700

    commit

--- PBL3/Models/TamtentoiContext.cs
+++ PBL3/Models/TamtentoiContext.cs
@@ -53,7 +53,7 @@ namespace PBL3.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=msi\\sqlexpress01;Database=Tamtentoi;Trusted_Connection=True;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=LAPTOP-CUA-DUY\\SQLEXPRESS02;Database=tamtentoiDB;Trusted_Connection=True;Integrated Security=True;");
             }
         }
 
